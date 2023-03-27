64d25f8b3748927539798e2b7b760f84b5a03da5
caselongo <caselongo@gmail.com>
2023-03-27 23:28:35 +0200

    Update Service.go

--- Service.go
+++ Service.go
@@ -5,23 +5,32 @@ import (
 	errortools "github.com/leapforce-libraries/go_errortools"
 	go_http "github.com/leapforce-libraries/go_http"
 	"net/http"
+	"regexp"
 )
 
 const (
 	apiName          string = "BAG"
 	apiURL           string = "https://api.bag.kadaster.nl/lvbag/individuelebevragingen/v2"
 	apiURLAcceptatie string = "https://api.bag.acceptatie.kadaster.nl/lvbag/individuelebevragingen/v2"
+	// this regex appears in error response if postcode has invalid format
+	// we use it in the function ValidatePostcode which provides a way to check the postcode format before calling the API itself
+	regexPostcode string = `^[1-9]{1}[0-9]{3}[ ]{0,1}[a-zA-Z]{2}$`
 )
 
 type Service struct {
 	apiKey        string
 	useAcceptatie bool
 	httpService   *go_http.Service
+	rPostcode     *regexp.Regexp
 }
 
 type ServiceConfig struct {
 	ApiKey        string
 	UseAcceptatie *bool
+}
+
+func (service *Service) ValidatePostcode(postcode string) bool {
+	return service.rPostcode.Match([]byte(postcode))
 }
 
 func NewService(config *ServiceConfig) (*Service, *errortools.Error) {
@@ -43,6 +52,7 @@ func NewService(config *ServiceConfig) (*Service, *errortools.Error) {
 		apiKey:        config.ApiKey,
 		useAcceptatie: useAcceptatie,
 		httpService:   httpService,
+		rPostcode:     regexp.MustCompile(regexPostcode),
 	}, nil
 }
 
